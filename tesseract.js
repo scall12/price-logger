29421d7d924ae07d47aeb7f16a02e0559f1d22b6
Spencer Call <spencergeraldcall@gmail.com>
2020-02-21 14:55:45 +0000

    Serve tesseract test file

---
+++ tesseract.js
@@ -0,0 +1,14 @@
+const worker = createWorker();
+
+(async () => {
+  await worker.load();
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+  const {
+    data: { text }
+  } = await worker.recognize(
+    'https://tesseract.projectnaptha.com/img/eng_bw.png'
+  );
+  console.log(text);
+  await worker.terminate();
+})();
