82522970abdbedf05d38f84cf12567e673adbb4f
Spencer Call <spencergeraldcall@gmail.com>
2020-02-21 13:59:28 +0000

    Edits

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const data = [];
 const submitBtn = document.querySelector('input[type="submit"]');
+const dropdown = document.querySelector('select');
+const itemInput = document.querySelector('#item');
+const priceInput = document.querySelector('#price');
+
+
 
 submitBtn.addEventListener('click', () => {
-    console.log('CLicked.')
+    const newData = {
+        store: dropdown.value,
+        data: {
+            item: itemInput.value,
+            price: priceInput.value
+        }
+    };
+    // console.log(newData);
+    if(data !== []){
+        for (obj in data) {
+            if (obj.store === newData.store){
+
+            } else {
+                data.push({store: newData.store})
+            }
+        };
+    };
 });
+
