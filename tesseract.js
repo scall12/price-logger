8271a658a2fed97abb06fb7fe168b90705954e38
Spencer Call <spencergeraldcall@gmail.com>
2020-02-24 12:58:53 +0000

    Test several receipt images in tesseract. Output data is unintelligible. Tesseract could still be an imporvement in the future, but tabled for now.

--- tesseract.js
+++ tesseract.js
@@ -1,14 +1,30 @@
-const worker = new.Tesseract.createWorker();
+// let requireModule = ( () => {
+//     return {
+//         requires = () => {
+//         const heic2any = require('heic2any');
+//         return heic2any;
+//         }
+//     }
+// })();
+
+// const image = 'https://tesseract.projectnaptha.com/img/eng_bw.png';
+// const image = '../images/test-receipt-img';
+const image = '../images/IMG_1008.jpg';
+
+const worker = new Tesseract.createWorker({
+  langPath: 'eng.traineddata.gz'
+});
 
 (async () => {
   await worker.load();
   await worker.loadLanguage('eng');
   await worker.initialize('eng');
+  //   await worker.setParameters({
+  //     tessedit_char_whitelist: 'abcde'
+  //   });
   const {
     data: { text }
-  } = await worker.recognize(
-    'https://tesseract.projectnaptha.com/img/eng_bw.png'
-  );
+  } = await worker.recognize(image);
   console.log(text);
   await worker.terminate();
 })();
