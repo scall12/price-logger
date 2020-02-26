02ef33bd2da29478fed9bda33926383be720394f
Spencer Call <spencergeraldcall@gmail.com>
2020-02-26 20:50:47 +0000

    Connect MongoDB Atlas cluster and post data to database upon form submission

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 const app = express();
 
+let db;
+const uri =
+  'mongodb+srv://user_1:xT2atnoDEHj66peU@cluster0-uupiu.mongodb.net/test?retryWrites=true&w=majority';
+
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -12,11 +18,23 @@ app.get('/', (req, res) => {
 
 app.listen(3010, () => {});
 
-//Connection URL
-url =
-  'mongodb+srv://user_1:xT2atnoDEHj66peU@cluster0-uupiu.mongodb.net/test?retryWrites=true&w=majority';
-
-MongoClient.connect(url, (err, client) => {
+const connectMongo = MongoClient.connect(uri, (err, client) => {
   assert.equal(null, err);
-  client.close();
+  db = client.db('price-logger');
+});
+
+app.post('/data', (req, res) => {
+  connectMongo;
+  const { store, item, price, options } = req.body;
+  db.collection(store)
+    .insertOne({
+      item,
+      price,
+      options
+    })
+    .then(result => {
+      client.close();
+    });
+
+  res.redirect('/');
 });
