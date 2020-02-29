8c7ad7b9f0a35bc39f25c8f6b23723011cec414b
Spencer Call <spencergeraldcall@gmail.com>
2020-02-29 20:10:32 +0000

    Receive results.json file with AJAX fetch. Create table to house results.

--- public/app.js
+++ public/app.js
@@ -1,6 +1,35 @@
-// const drop = document.querySelector('#store');
-// const collections = [];
+const searchBar = document.querySelector('#search-bar');
+const chartDiv = document.querySelector('#chart');
 
-// for (let store of drop) {
-//   collections.push(store);
-// }
+searchBar.addEventListener('keydown', event => {
+  // Enter key press
+  if (event.keyCode && event.keyCode === 13) {
+    fetch('/search/results.json')
+      .then(response => response.json())
+      .then(data => {
+        let table = document.createElement('table');
+        let thead = document.createElement('thead');
+        let tr = document.createElement('tr');
+        let tbody = document.createElement('tbody');
+        let th = document.createElement('th');
+
+        chartDiv.appendChild(table);
+        table.append(thead, tbody);
+        thead.append(tr);
+        tr.append(th);
+
+        for (let obj of data) {
+          let tr = document.createElement('tr');
+          tbody.append(tr);
+          for (let atr in obj) {
+            let td = document.createElement('td');
+            tr.append(td);
+          }
+        }
+
+        let p = document.createElement('p');
+        chartDiv.appendChild(p);
+        p.innerHTML = JSON.stringify(data[0].item);
+      });
+  }
+});
