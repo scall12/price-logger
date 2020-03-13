32e78ac1be78c2ff67173e54aa6ff0552779e8f9
Spencer Call <spencergeraldcall@gmail.com>
2020-03-13 13:11:46 +0000

    Initial successful integration of OKTA authentication.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyparser = require('body-parser');
+const { ExpressOIDC } = require('@okta/oidc-middleware');
+const session = require('express-session');
 
 const searchRouter = require('./routes/search');
 const inputRouter = require('./routes/input');
@@ -10,13 +12,37 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(
+  session({
+    secret: process.env.APP_SECRET,
+    resave: true,
+    saveUninitialized: false
+  })
+);
+
+const oidc = new ExpressOIDC({
+  issuer: `${process.env.ORG_URL}/oauth2/default`,
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  redirect_uri: `${process.env.HOST_URL}/authorization-code/callback`,
+  scope: 'openid profile',
+  appBaseUrl: process.env.HOST_URL
+});
+
+app.use(oidc.router);
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(searchRouter);
 app.use(inputRouter);
 
 app.get('/', (req, res) => {
-  res.render(__dirname + '/views/index.ejs');
+  res.sendFile(__dirname + '/views/home.html');
 });
 
-app.listen(3010, () => {});
+oidc.on('ready', () => {
+  app.listen(3010, () => console.log('Started'));
+});
+
+oidc.on('error', err => {
+  console.log('Unable to configure ExpressOIDC', err);
+});
